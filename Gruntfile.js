d731b9479891fe6571b69646f5533cb050957dbc
Mark Odell <mark.odell@sjsu.edu>
2016-02-07 11:56:21 -0800

    Have created framework for building js and css using SASS, Grunt. All that is left is to creat a way of generating HTML content.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,16 +5,16 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     
     watch: {
+
       scripts: {
-        files: ['stylesheets/*.scss'],
-        tasks: ['sass:build', 'uglify:build'],
-        options: {
-          spawn: false,
-        },
+        files: ['assets/stylesheets/**/*.scss', 'assets/javascripts/**/*.js'], // Watch scss & js files for changes
+        tasks: ['sass:build', 'uglify:build'], // Build sass and js files
       },
+
     }, // watch
 
     uglify: {
+
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
@@ -22,34 +22,56 @@ module.exports = function(grunt) {
         src: 'assets/javascript/sjsu.js',
         dest: 'build/assets/javascripts/sjsu.min.js'
       }
+
     }, // uglify
 
-    sass: {                              // Task
+    sass: {
       
-      build: {                           // Target
-        options: {                       // Target options
-          style: 'expanded'
+      build: {
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+          style: 'expanded',
+          trace: true,
+          loadPath: [
+            // This allows me to import scss files from this directory.
+            'vendor/bootstrap/assets/stylesheets'
+          ]
         },
-        files: {                         // Dictionary of files
+        files: {  // Dictionary of files
           // 'destination': ['sources']
           'build/assets/stylesheets/sjsu.css': ['assets/stylesheets/sjsu.scss'],
+          'build/assets/stylesheets/bootstrap.css': 'assets/stylesheets/sjsu-bootstrap.scss',
         }
       }
 
-    } // sass
+    }, // sass
+
+    browserSync: {
+      
+      dev: {
+        bsfiles: { // Files you would like to watch for changes to refresh browser page
+          src: [
+            'build/assets/stylesheets/*.css',
+            'build/assets/javascripts/*.js',
+            'build/*.html'
+          ]
+        },
+        option: {
+          watchTask: true,
+          server: './build'
+        }
+      }
+
+    } // browserSync
 
   });
 
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify:build']);
-  grunt.registerTask('watch', ['watch:scripts']);
-  grunt.registerTask('buildjs', ['uglify:build']);
-  grunt.registerTask('buildcss', ['sass:build']);
-
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('build', ['uglify:build', 'sass:build']);
+  grunt.registerTask('test', ['browserSync', 'watch']);
 };
